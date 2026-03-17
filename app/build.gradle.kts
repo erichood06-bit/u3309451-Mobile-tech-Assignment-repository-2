f3f9d8653f2ce9c1455247d25225290231d8e6e0
Eric <erichood06@gmail.com>
2026-03-17 13:20:05 +1100

    Wk5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -40,6 +41,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     implementation("com.google.android.gms:play-services-location:21.3.0")
     androidTestImplementation(libs.ext.junit)
